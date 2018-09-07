ac421b1da3596aa98c6cdb7481f3ff67adf41f3e
Lucas Basquerotto <lucasbasquerotto@gmail.com>
2018-09-07 20:04:03 -0300

    Create prep-main.sh

---
+++ prep-main.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+VERSION="$1"
+  
+if [ -z "$VERSION" ]
+then
+  echo "No tag supplied"
+  exit 1
+fi
+
+echo "updating temporary directory to tag $VERSION..."
+main update .tmp "$VERSION"
+
+echo "pulling up-to-date images..."
+cd /docker/.tmp
+docker-compose pull
+
+echo "preparation finished"
